c191a880b0d6ce92badf00b75d6b3b97be1d5ddf
henrique.roldao <henrique.roldao@betha.com.br>
2022-03-12 12:16:58 -0300

    Aula 02 - Começando a botar POO em prática com c#

---
+++ Aula02TesteAmbiente/Entities/Autor.cs
@@ -0,0 +1,19 @@
+namespace Aula02TesteAmbiente.Entities
+{
+    public class Autor
+    {
+
+        public Autor(int id, string nome)
+        {
+            this.Id = id;
+            this.Nome = nome;
+        }
+
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Endereco { get; set; }
+
+
+
+    }
+}
--- Aula02TesteAmbiente/Program.cs
+++ Aula02TesteAmbiente/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Aula02TesteAmbiente.Entities;
 
 namespace Aula02TesteAmbiente
 {
@@ -7,6 +8,17 @@ namespace Aula02TesteAmbiente
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Autor oAutor = new Autor(128, "Hisuy");
+
+            Livro objLivro = new Livro(100, "Banco de dados", oAutor);  
+            Console.WriteLine(
+                objLivro.GetId() + " - " + 
+                objLivro.GetTitulo() + " - " + 
+                objLivro.GetValor() + " - " +
+                objLivro.Autor.Nome);
+
+            objLivro.SetValor(199);
+            Console.WriteLine("valor do livro: R$" + objLivro.GetValor());
         }
     }
 }
